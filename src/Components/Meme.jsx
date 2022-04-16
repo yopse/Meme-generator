49d25e038518444e90a3d72d826e15e58ca82de8
yopse <82827725+yopse@users.noreply.github.com>
2022-04-17 01:55:27 +0530

    refactor

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,39 +1,45 @@
 import React from "react";
 import memeData from "../memeData";
 
-
-
 const Meme = () => {
+  // let [imageUrl, setImageUrl] = React.useState("")
 
+  let [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    let [imageUrl, setImageUrl] = React.useState("")
+  let [allMemeImages, setAllMemeImages] = React.useState(memeData);
 
   const getMemeImage = () => {
+    let a = allMemeImages.data.memes;
+    let randomNumber = Math.floor(Math.random() * a.length);
+    let url = a[randomNumber].url;
 
-     let a = memeData.data.memes;
-     let randomNumber = Math.floor(Math.random() *  a.length);
-                //  memesArray[randomNumber].url
-        setImageUrl(a[randomNumber].url);
-  }
-
-
-
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        randomImage: url,
+      };
+    });
+  };
 
   return (
     <>
       <div className="meme-box">
-          <div>
-
-              <div className="input-box">
-              <input type="text" placeholder="Shut up"></input>
-              <input type="text" placeholder="and take my money"></input>
-              </div>
-              
-              <button className="btn" onClick={getMemeImage}>Get a new meme image</button>
-             
+        <div>
+          <div className="input-box">
+            <input type="text" placeholder="Shut up"></input>
+            <input type="text" placeholder="and take my money"></input>
           </div>
-           
-              <img src={imageUrl} className="meme-image"></img>
+
+          <button className="btn" onClick={getMemeImage}>
+            Get a new meme image
+          </button>
+        </div>
+
+        <img src={meme.randomImage} alt="co" className="meme-image"></img>
       </div>
     </>
   );
